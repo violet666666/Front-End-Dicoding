089dc9f254b344783e788b3b3e7c9ba728ca829a
violet666666 <setanhantu11111@gmail.com>
2022-07-28 19:08:23 +0700

    update calculator js till complete

--- Web-Calculator/assets/kalkulator.js
+++ Web-Calculator/assets/kalkulator.js
@@ -91,3 +91,34 @@ function performCalculation() {
 
     calculator.displayNumber = result;
 }
+const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result
+}
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+    }
+
+    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
+    calculator.displayNumber = result;
+    renderHistory();
+}
