1afb8b1528cff59dd0f778ba5b6bab13d72506ea
violet666666 <setanhantu11111@gmail.com>
2022-07-30 01:27:11 +0700

    add banner and about section

---
+++ Submission Akhir/js/script.js
@@ -0,0 +1,21 @@
+const txtElement = ['Freelancer', 'Web Developer', 'Graphic Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentText = '';
+let words = '';
+
+(function ketik() {
+    if (count == txtElement.length) {
+        count = 0;
+    }
+    currentText = txtElement[count];
+    words = currentText.slice(0, ++txtIndex);
+    document.querySelector('.efek-ketik').textContent = words;
+
+    if (words.length == currentText.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ketik, 500);
+})();
